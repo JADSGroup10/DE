e0ef3d463c6f5add0a620bf58591142fcf8edfb3
wmu66 <weiler.marko@gmail.com>
2025-10-14 11:09:33 +0200

    Changed ports

--- prediction-ui/app.py
+++ prediction-ui/app.py
@@ -50,4 +50,4 @@ def checksurvivor():
 
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5003)), host='0.0.0.0', debug=True)
+    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
